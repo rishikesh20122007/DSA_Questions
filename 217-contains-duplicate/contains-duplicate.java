7eaee282121c68d6e33f043732094e3e2eb43fd5
Rishikesh Kumar <rishikesh021200@gmail.com>
2026-09-03 23:54:14 +0530

    Time: 41 ms (5.13%) | Memory: 110.5 MB (10.11%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        for(int num:nums){
+            freq.put(num,freq.getOrDefault(num,0) +1);
+        }
+        for(int key:freq.keySet()){
+            int currfreq = freq.get(key);
+            if(currfreq > 1) return true;
+        }
+        return false;
+    }
+}
