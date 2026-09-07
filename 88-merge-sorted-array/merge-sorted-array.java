97051537c4d773389f23405eb9429611a32f96d3
Rishikesh Kumar <rishikesh021200@gmail.com>
2026-09-07 21:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (63.53%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,12 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       int i = m - 1;
-        // j: last index in nums2
-        int j = n - 1;
-        // k: last index in nums1 including extra 0s
+       int i = m - 1;   // j: last index in nums2
+        int j = n - 1;    // k: last index in nums1 including extra 0s
         int k = m + n - 1;
-        // Fill nums1 from the back
-        while (i >= 0 && j >= 0) {
+        while (i >= 0 && j >= 0) {     // Fill nums1 from the back
             // Place larger element from end of nums1 or nums2
             if (nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
