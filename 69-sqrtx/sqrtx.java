9ba20a8999ebf759d2fa1f66455649b62f48bb3a
Rishikesh Kumar <rishikesh021200@gmail.com>
2026-09-24 22:38:48 +0530

    Time: 1 ms (99.77%) | Memory: 42.6 MB (61.26%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x < 2) return x;
+
+        int start = 1;
+        int end = x / 2;
+        int ans = 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(mid <= x / mid){
+                ans = mid;
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
